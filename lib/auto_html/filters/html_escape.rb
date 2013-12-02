76db8f69709c785a8bba969e4297a8cfdda32bcb
Seo Cahill <seo.cahill@gmail.com>
2013-12-02 12:49:16 +0100

    escape right angle bracket

--- lib/auto_html/filters/html_escape.rb
+++ lib/auto_html/filters/html_escape.rb
@@ -1,8 +1,9 @@
 AutoHtml.add_filter(:html_escape).with(
   :map => { 
-    '&' => '&amp;',  
+    '&' => '&amp;',
+    '>' => '&gt;',
     '<' => '&lt;',
     '"' => '&quot;' }) do |text, options|
 
-  text.to_s.gsub(/[&"<]/) { |special| options[:map][special] }
+  text.to_s.gsub(/[&"><]/) { |special| options[:map][special] }
 end
