98e63549fd2fad063fae3cfe025746b16b5b1524
Seo Cahill <seo.cahill@gmail.com>
2015-05-20 14:40:05 +0200

    Update soundcloud.rb
    ssl only

--- lib/auto_html/filters/soundcloud.rb
+++ lib/auto_html/filters/soundcloud.rb
@@ -6,7 +6,7 @@ AutoHtml.add_filter(:soundcloud).with({}) do |text, options|
   # :maxwidth => '', :maxheight => '', :auto_play => false, :show_comments => false
   text.gsub(/(https?:\/\/)?(www.)?soundcloud\.com\/.*/) do |match|
     new_uri = match.to_s
-    new_uri = (new_uri =~ /^https?\:\/\/.*/) ? URI(new_uri) : URI("http://#{new_uri}")
+    new_uri = URI(new_uri)
     new_uri.normalize!
 
     uri = URI("http://soundcloud.com/oembed")
